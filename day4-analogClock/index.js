866e7f170b36e4129af8521d9eeb8118a47a5e52
Cosmin <cosminotet0@gmail.com>
2019-10-01 22:13:27 +0300

    day4

---
+++ day4-analogClock/index.js
@@ -0,0 +1,28 @@
+const clockCircle = document.querySelector('.clockCircle');
+
+const secondHand = document.querySelector('.sec-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.h-hand');
+
+
+
+function setDate() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    
+
+    const minutes = now.getMinutes();
+    const minuteDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+
+setDate();
